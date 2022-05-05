344ade17d353f9f0c5db5e645fb5df0a00ece896
6879656a696e <6879656a696e@gmail.com>
2022-05-05 18:16:03 +0900

    20220505

---
+++ Programmers/KAKAO/2020 KAKAO BLIND RECRUITMENT/문자열 압축.java
@@ -0,0 +1,29 @@
+import java.util.*;
+class Solution {
+    public int solution(String s) {
+        int answer = s.length();
+        for(int i=1; i<=s.length()/2; i++) {
+            String subs=s.substring(0,i);
+            StringBuilder result=new StringBuilder();
+            int cnt=1;
+            for(int j = i; j < s.length(); j += i){
+                String subComp = s.substring(j, j + i > s.length() ? s.length() : i + j);
+
+                if(subs.equals(subComp)){
+                    cnt++;
+                } else {
+                    result.append((cnt !=1 ? cnt : "")+subs);
+                    subs=subComp;
+                    cnt=1;
+                }
+            }
+            if(cnt != 1) {
+                result.append(cnt+subs);
+            } else {
+                result.append(subs);
+            }
+            answer=Math.min(answer, result.length());
+        }
+        return answer;
+    }
+}

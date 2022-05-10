81968b0bdda46847ca9856c052e63b900fc5a1e8
6879656a696e <97209511+6879656a696e@users.noreply.github.com>
2022-05-10 11:22:04 +0900

    [Gold III] Title: 나머지 합, Time: 560 ms, Memory: 115300 KB -BaekjoonHub

---
+++ 백준/Gold/10986. 나머지 합/나머지 합.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int sum=0;
+        int[] count = new int[m];
+        st=new StringTokenizer(br.readLine());
+        for(int i=0;i<n;i++){
+            sum=(sum+Integer.parseInt(st.nextToken()))%m;
+            count[sum]++;
+        }
+        long ans=count[0];
+        for(int i=0;i<m;i++){
+            ans+=(long) count[i]*(count[i]-1)/2;
+        }
+        System.out.println(ans);
+    }
+}

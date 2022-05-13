fb2700d28be5627c68642b872b8ab89bc8ba2557
6879656a696e <97209511+6879656a696e@users.noreply.github.com>
2022-05-13 12:08:19 +0900

    [Silver I] Title: RGB거리, Time: 272 ms, Memory: 20556 KB -BaekjoonHub

---
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class Main{
+    final static int red=0;
+    final static int green=1;
+    final static int blue=2;
+    static int[][] cost;
+    static int[][] dp;
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        cost=new int[n][3];
+        dp=new int[n][3];
+
+        for(int i=0;i<n;i++){
+            cost[i][red]=sc.nextInt();
+            cost[i][green]=sc.nextInt();
+            cost[i][blue]=sc.nextInt();
+        }
+        dp[0][red]=cost[0][red];
+        dp[0][green]=cost[0][green];
+        dp[0][blue]=cost[0][blue];
+        System.out.print(Math.min(Paint_cost(n- 1, red), Math.min(Paint_cost(n - 1, green), Paint_cost(n - 1, blue))));
+    }
+    static int Paint_cost(int n, int color){
+        if(dp[n][color]==0){
+            if(color==red){
+                dp[n][red]=Math.min(Paint_cost(n-1,green), Paint_cost(n-1,blue))+cost[n][red];
+            } else if(color==green){
+                dp[n][green]=Math.min(Paint_cost(n-1,red), Paint_cost(n-1,blue))+cost[n][green];
+            } else {
+                dp[n][blue]=Math.min(Paint_cost(n-1,red), Paint_cost(n-1,green))+cost[n][blue];
+            }
+        }
+        return dp[n][color];
+    }
+}
